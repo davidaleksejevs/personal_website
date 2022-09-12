db05b6fe72d18f37da09a83c31402662dc1d5a7b
davidaleksejevs <david.aleksejevs@gmail.com>
2022-09-12 13:10:43 +0100

    added react front end
    changed src added app.js and index.js also eddited index.html

--- jsmportfolio/schemas/testimonials.js
+++ jsmportfolio/schemas/testimonials.js
@@ -1,30 +1,30 @@
 export default {
-  name: "testimonials",
-  title: "Testimonials",
-  type: "document",
-  fields: [
-    {
-      name: "name",
-      title: "name",
-      type: "string",
-    },
-    {
-      name: "company",
-      title: "Company",
-      type: "string",
-    },
-    {
-      name: "imageurl",
-      title: "ImageUrl",
-      type: "image",
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: "feedback",
-      title: "FeedBack",
-      type: "string",
-    },
-  ],
-};
+    name:'testimonials',
+    title:'Testimonials',
+    type: 'document',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
+            type: 'string'
+        },
+        {
+            name:'company',
+            title:'Company',
+            type:'string'
+        },
+        {
+            name:'imgurl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },  
+        {
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
+        }
+    ]
+}
